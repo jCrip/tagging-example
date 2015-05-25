d20047fa7751164ebd395639af5c1f67a88df7dc
Juan Cristobal Pazos <juancri@lalegion.cl>
2015-05-25 14:35:43 -0300

    tagging example project

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,26 @@
+class PostsController < ApplicationController
+  def index
+    if params[:tag]
+      @posts = Post.tagged_with(params[:tag])
+    else
+      @posts = Post.all
+    end
+  end
+
+  def create
+    @post = Post.new(post_params)
+    respond_to do |format|
+      if @post.save
+        format.js # Will search for create.js.erb
+      else
+        format.html { render root_path }
+      end
+    end
+  end
+
+  private
+
+    def post_params
+      params.require(:post).permit(:author, :content, :all_tags)
+    end
+end
